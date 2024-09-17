0a32a25ce524d0b4b59dadfcf3f4bece5c49ea38
Dbalu15 <deepikabalu15@gmail.com>
2024-09-18 00:16:20 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float val1 = 91.0f;
+		String val2 ="Google";
+		boolean val3 = false;
+		int year = 2008;
+		char val5 = 'C';
+		
+		System.out.println(+val1 +val2+ val3+ year +val5);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+import week1.day2.LearnMethods;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to TestLeaf");
+		LearnMethods lc=new LearnMethods();
+		lc.browserVersion();
+	}
+
+}
